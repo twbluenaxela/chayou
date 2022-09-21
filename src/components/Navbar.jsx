dc8e0678ff4609020c0dc6fbe215419f12bae903
于樂洋 <rednaxela64@outlook.com>
2022-09-21 16:29:58 -0700

    init project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { 
+    Nav, 
+    Navbar, 
+    NavDropdown,
+    Container } from 'react-bootstrap'
+
+    const NavigationBar = () => {
+        return (
+            <Navbar>
+                <Container>
+                    <Navbar.Brand>茶友</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav' >
+                        <Nav className='me-auto' >
+                            <Nav.Link href='#'>首页</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+
+export default NavigationBar
---
+++ src/views/Homepage.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+
+const Homepage = () => {
+    return (
+        <div>
+        </div>
+    )
+}
